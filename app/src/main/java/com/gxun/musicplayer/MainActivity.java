e92c3a4098e9038e2cbc09e2087cdd55505c080c
Jeff <1820779716@qq.com>
2020-06-15 00:10:38 +0800

    完善功能：点击选歌，调节进度条 ListView：添加点击事件，点击选歌 SeekBar：监听进度条事件，移动调整进度

--- app/src/main/java/com/gxun/musicplayer/MainActivity.java
+++ app/src/main/java/com/gxun/musicplayer/MainActivity.java
@@ -40,9 +40,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     //定义控制动作、更新动作
     public static final String CTRL_ACTION = "com.gxun.musicplayer.CTRL_ACTION";
     public static final String UPDATE_ACTION = "com.gxun.musicplayer.UPDATE_ACTION";
+    public static final String CURRENT_POSITION_ACTION = "com.gxun.musicplayer.DURATION_ACTION";
+    public static final String SEEKBAR_PROGRESS_ACTION = "com.gxun.musicplayer.SEEKBAR_PROGRESS_ACTION";
+    public static final String CLICKITEM_ACTION = "com.gxun.musicplayer.CLICKITEM_ACTION";
 
-    // 定义音乐的播放状态，0x11代表没有播放，0x12代表正在播放，0x13代表暂停
-    int status = 0x11;
+    // 定义音乐的播放状态，stop代表没有播放，playing代表正在播放，pause代表暂停
+    String status = "stop";
+
+    int currentPosition, musicMaxTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,14 +84,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         initialMusicList(); // 初始化音乐列表
         initialSeekBar(); // 初始化进度条
-        // 设置播放信息栏
-        musicInfoTv.setText("当前无播放");
+
+        musicInfoTv.setText("当前无播放"); // 设置播放信息
 
         activityReceiver = new ActivityReceiver();
         // 创建IntentFilter
         IntentFilter filter = new IntentFilter();
         // 指定BroadcastReceiver监听的Action
-        filter.addAction(UPDATE_ACTION); // 更新动作，用于接收MusicService回传的Intent
+        filter.addAction(UPDATE_ACTION); // 更新动作，用于接收MusicService回传的音乐信息的Intent
+        filter.addAction(CURRENT_POSITION_ACTION); // 更新动作，用于接收MusicService回传的进度条信息的Intent
         // 注册BroadcastReceiver
         registerReceiver(activityReceiver, filter);
         final Intent intent = new Intent(this, MusicService.class);
@@ -97,76 +103,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         musicListLv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+                Intent itemIntent = new Intent(CLICKITEM_ACTION);
+                itemIntent.putExtra("musicNum", position);
+                sendBroadcast(itemIntent);
             }
         });
 
         // 进度条实现
-//        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { //进度条
-//            @Override
-//            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) { // 用于监听SeekBar进度值的改变
-//
-//            }
-//
-//            @Override
-//            public void onStartTrackingTouch(SeekBar seekBar) { // 用于监听SeekBar开始拖动
-//
-//            }
-//
-//            @Override
-//            public void onStopTrackingTouch(SeekBar seekBar) { // 用于监听SeekBar停止拖动  SeekBar停止拖动后的事件
-//                int progress = seekBar.getProgress();
-//                Log.i("TAG:", "" + progress + "");
-//                int musicMaxTime = intent.getIntExtra("maxTime",-1); // 得到该首歌曲最长秒数
-//                int seekBarMax = seekBar.getMax();
-//                double currentProcess = musicMaxTime * progress / seekBarMax;
-//                musicService.player.seekTo(); // 跳到该曲该秒
-//            }
-//        });
-//        Thread t = new Thread() { // 自动改变进度条的线程
-//            @Override
-//            public void run() {
-//                int position, mMax, sMax;
-//                while (!Thread.currentThread().isInterrupted()) { // 线程未结束会一直返回false，此时可以一直刷新进度条
-//                    if (musicService.player != null && musicService.player.isPlaying()) {
-//                        position = musicService.getCurrentProgress(); // 得到当前歌曲播放进度(秒)
-//                        mMax = musicService.player.getDuration(); // 最大秒数
-//                        sMax = seekBar.getMax();// seekBar最大值，算百分比
-//                        Message m = handler.obtainMessage(); // 获取一个Message
-//                        m.arg1 = position * sMax / mMax; // seekBar进度条的百分比
-//                        m.arg2 = position; // 当前位置
-//                        m.what = status; // 状态
-//                        handler.sendMessage(m);
-//                        try {
-//                            Thread.sleep(1000);// 每间隔1秒发送一次更新消息
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }
-//            }
-//        };
-//        // 实例化一个handler对象
-//        // 通过 Handler 更新 UI 上的组件状
-//        handler = new Handler() {
-//            @Override
-//            public void handleMessage(Message msg) {
-//                super.handleMessage(msg);
-//                int mMax = musicService.player.getDuration(); // 最大秒数
-//                if (msg.what == status) {
-//                    try {
-//                        seekBar.setProgress(msg.arg1);
-//                        setPlayTime(msg.arg2 / 1000, mMax / 1000);
-//                    } catch (Exception e) {
-//                        e.printStackTrace();
-//                    }
-//                } else {
-//                    seekBar.setProgress(0);
-//                    musicInfoTv.setText("播放已经停止");
-//                }
-//            }
-//        };
-//        t.start(); //启动线程
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() { //进度条
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) { // 用于监听SeekBar进度值的改变
+                if (fromUser){
+                    int seekBarMax = seekBar.getMax();
+                    sendProgress(progress, seekBarMax);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { // 用于监听SeekBar开始拖动
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { // 用于监听SeekBar停止拖动  SeekBar停止拖动后的事件
+            }
+        });
+
+    }
+
+    private void sendProgress(int progress, int seekBarMax) {
+        Intent progressIntent = new Intent(SEEKBAR_PROGRESS_ACTION);
+        progressIntent.putExtra("Progress", progress);
+        progressIntent.putExtra("SeekBarMax", seekBarMax);
+        sendBroadcast(progressIntent);
     }
 
     @Override
@@ -216,58 +184,75 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         seekBar.setProgress(0); // 进度条置0
     }
 
-    // 设置当前播放的信息
-    void setPlayTime(int position, int max) {
-
-        int pMinutes = 0;
-        while (position >= 60) {
-            pMinutes++;
-            position -= 60;
-        }
-
-        String currentTime = (pMinutes < 10 ? "0" + pMinutes : pMinutes) + ":"
-                + (position < 10 ? "0" + position : position);
-        musicCurrentTimeTv.setText(currentTime);
-
+    // 设置总播放时间
+    void setTotalTime(int max){
         int mMinutes = 0;
         while (max >= 60) {
             mMinutes++;
             max -= 60;
         }
-
         String totalTime = (mMinutes < 10 ? "0" + mMinutes : mMinutes) + ":"
                 + (max < 10 ? "0" + max : max);
         musicTotalTimeTv.setText(totalTime);
+    }
+    //设置当前时间及进度条
+    void setCurrentTime(int position) {
+        int pMinutes = 0;
+        while (position >= 60) {
+            pMinutes++;
+            position -= 60;
+        }
+        String currentTime = (pMinutes < 10 ? "0" + pMinutes : pMinutes) + ":"
+                + (position < 10 ? "0" + position : position);
+        musicCurrentTimeTv.setText(currentTime);
+        seekBar.setProgress(currentPosition * seekBar.getMax() / musicMaxTime);
     }
 
     //自定义BroadcastReceiver，负责监听从Service传回来的广播
     public class ActivityReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()){
+                case UPDATE_ACTION:
+                    updateStatus(intent);
+                    break;
+                case CURRENT_POSITION_ACTION:
+                    updateSeekBar(intent);
+                    break;
+            }
+        }
+        public void updateStatus(Intent intent){
             // 获取Intent中的newStatus（新的播放状态）
-            int newStatus = intent.getIntExtra("newStatus", -1);
+            String newStatus = intent.getStringExtra("newStatus");
             // 获取Intent中的musicName（当前播放的歌曲名）
             String musicName = intent.getStringExtra("musicName");
+            musicMaxTime = intent.getIntExtra("maxTime", 0);
             String musicInfo = "";
             switch (newStatus) {
-                case 0x11: // 无播放状态
+                case "stop": // 无播放状态
                     startBtn.setText("开始");
                     musicInfo = "当前无播放";
                     initialSeekBar();
-                    status = 0x11;
+                    status = newStatus;
                     break;
-                case 0x12: // 正在播放状态
+                case "playing": // 正在播放状态
                     startBtn.setText("暂停");
                     musicInfo = "正在播放: " + musicName;
-                    status = 0x12;
+                    status = newStatus;
                     break;
-                case 0x13: // 暂停状态
+                case "pause": // 暂停状态
                     startBtn.setText("开始");
                     musicInfo = "播放暂停: " + musicName;
-                    status = 0x13;
+                    status = newStatus;
                     break;
             }
             musicInfoTv.setText(musicInfo);
+            setTotalTime(musicMaxTime/1000); // 广播返回的为ms，需转换为s
+        }
+
+        public void updateSeekBar(Intent intent){
+            currentPosition = intent.getIntExtra("currentPosition", 0);
+            setCurrentTime(currentPosition/1000);
         }
     }
 }
--- app/src/main/java/com/gxun/musicplayer/MusicService.java
+++ app/src/main/java/com/gxun/musicplayer/MusicService.java
@@ -14,17 +14,25 @@ import androidx.annotation.Nullable;
 
 import java.io.File;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MusicService extends Service {
 
-    private List<String> musicList;// 存放找到的所有mp3的绝对路径
-    private MediaPlayer player; // 定义多媒体对象
+    private List<String> musicList; // 存放找到的所有mp3的绝对路径
+    public MediaPlayer player; // 定义多媒体对象
     private int musicNum; // 当前播放的歌曲在List中的下标
     private String musicName; // 当前播放的歌曲名
 
     MusicServiceReceiver musicServiceReceiver; // 此类中的广播接收器
 
-    int status = 0x11; // 多媒体对象初始状态
+    String status = "stop"; // 多媒体对象初始状态
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 
     @Override
     public void onCreate() {
@@ -33,57 +41,96 @@ public class MusicService extends Service {
         musicServiceReceiver = new MusicServiceReceiver();
         IntentFilter filter = new IntentFilter();
         filter.addAction(MainActivity.CTRL_ACTION); // 控制动作
+        filter.addAction(MainActivity.SEEKBAR_PROGRESS_ACTION); // 进度条动作
+        filter.addAction(MainActivity.CLICKITEM_ACTION);
         registerReceiver(musicServiceReceiver, filter);
 
         musicList = new MusicScanner().getMusicList(); // 获得mp3文件路径
         player = new MediaPlayer(); // 初始化多媒体对象
-    }
+        musicNum = 0; // 歌曲下标默认为0
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+        // 定时发送进度条信息
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                if(player.isPlaying()){
+                    sendCurrentPosition();
+                }
+            }
+        }, 0, 30);
     }
 
     // 创建广播子类接收消息
     public class MusicServiceReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
+            switch (intent.getAction()){
+                case MainActivity.CTRL_ACTION:
+                    receiveCtrl(intent);
+                    break;
+                case MainActivity.SEEKBAR_PROGRESS_ACTION:
+                    setProgress(intent);
+                    break;
+                case MainActivity.CLICKITEM_ACTION:
+                    musicNum = intent.getIntExtra("musicNum", -1);
+                    play();
+                    sendUpdateIntent();
+                    break;
+            }
+        }
+
+        public void receiveCtrl(Intent intent){
             int control = intent.getIntExtra("control",-1);
             switch (control){ // 控制状态：播放、暂停、停止、上一曲、下一曲
                 case 1:
-                    if(status == 0x11){ // 原来处于没有播放状态,准备并播放音乐
+                    if(status.equals("stop")){ // 原来处于没有播放状态,准备并播放音乐
                         play();
-                        status = 0x12; // 状态改为正在播放
-                    } else if(status == 0x12){ // 原来处于播放状态，转为暂停
+                    } else if(status.equals("playing")){ // 原来处于播放状态，转为暂停
                         pause();
-                        status = 0x13; // 状态改为暂停
-                    } else if(status == 0x13){ // 原来处于暂停状态
+                    } else if(status.equals("pause")){ // 原来处于暂停状态
                         goPlay();
-                        status = 0x12; // 状态改为正在播放
                     }
                     break;
                 case 2:
                     stop(); // 停止播放
-                    status = 0x11; // 状态设置为无播放状态
                     break;
                 case 3:
                     last(); // 上一曲
-                    status = 0x12; // 状态改为正在播放
                     break;
                 case 4:
                     next(); // 下一曲
-                    status = 0x12; // 状态改为正在播放
                     break;
             }
-            // 广播通知Activity更改图标、文本框
-            Intent updateIntent = new Intent(MainActivity.UPDATE_ACTION); // 指定更新动作
-            updateIntent.putExtra("newStatus",status); // 发送新状态
-            updateIntent.putExtra("musicName", musicName); // 发送音乐文件名
-            updateIntent.putExtra("maxTime", player.getDuration()); // 该首歌曲最长秒数
-            // 发送广播，将被MainActivity组件中的BroadcastReceiver接收
-            sendBroadcast(updateIntent);
+            sendUpdateIntent();
         }
+
+        public void setProgress(Intent intent){
+            if (player!=null && player.isPlaying()){
+                int progress = intent.getIntExtra("Progress",-1);
+                int seekBarMax = intent.getIntExtra("SeekBarMax", -1);
+                int musicMax = player.getDuration();
+                player.seekTo(musicMax * progress / seekBarMax);
+                sendCurrentPosition();
+            }
+        }
+    }
+
+    public void sendUpdateIntent(){
+        // 广播通知MainActivity音乐信息
+        Intent updateIntent = new Intent(MainActivity.UPDATE_ACTION); // 指定更新动作
+        updateIntent.putExtra("newStatus",status); // 发送新状态
+        updateIntent.putExtra("musicName", musicName); // 发送音乐文件名
+        updateIntent.putExtra("maxTime", player.getDuration()); // 发送歌曲时长，时间为ms
+        // 发送广播，将被MainActivity组件中的BroadcastReceiver接收
+        sendBroadcast(updateIntent);
+    }
+
+    public void sendCurrentPosition(){
+        // 广播通知MainActivity当前进度
+        Intent positionIntent = new Intent(MainActivity.CURRENT_POSITION_ACTION);
+        positionIntent.putExtra("currentPosition", getCurrentProgress());
+        sendBroadcast(positionIntent);
     }
 
     public void setPlayName(String dataSource) {
@@ -97,7 +144,7 @@ public class MusicService extends Service {
     public void play() {
         try {
             player.reset(); // 重置多媒体
-            String dataSource = musicList.get(musicNum);// 得到当前播放音乐的路径
+            String dataSource = musicList.get(musicNum); // 得到当前播放音乐的路径
             setPlayName(dataSource); // 截取歌名
             // 指定参数为音频文件
             player.setAudioStreamType(AudioManager.STREAM_MUSIC);
@@ -107,8 +154,10 @@ public class MusicService extends Service {
             player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 public void onCompletion(MediaPlayer arg0) {
                     next(); // 如果当前歌曲播放完毕,自动播放下一首
+                    sendUpdateIntent(); // 广播通知Activity
                 }
             });
+            status = "playing"; // 状态改为正在播放
         } catch (Exception e) {
             Log.i("MusicService", e.getMessage());
         }
@@ -123,6 +172,7 @@ public class MusicService extends Service {
             e.printStackTrace();
         }
         player.start();
+        status = "playing"; // 状态改为正在播放
     }
 
     public int getCurrentProgress() { // 获取当前进度
@@ -148,14 +198,16 @@ public class MusicService extends Service {
     public void pause() { // 暂停播放
         if (player != null && player.isPlaying()){
             player.pause();
+            status = "pause"; // 状态改为暂停
         }
     }
 
     public void stop() { //停止播放
         if (player != null && player.isPlaying()) {
             player.stop(); //停止播放
+            player.release();
             musicNum = 0; //重置歌曲下标
-            player.reset(); //重置多媒体对象
+            status = "stop"; // 状态设置为无播放状态
         }
     }
 }
